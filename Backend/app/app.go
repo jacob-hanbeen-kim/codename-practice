82ae31fa963081577cadac0d766f083d1dd47e58
Daniel Lee <daniellee@Daniels-MacBook-Air.local>
2022-10-28 23:11:58 -0400

    implement gin-gonic for routing

--- Backend/app/app.go
+++ Backend/app/app.go
@@ -2,12 +2,18 @@ package app
 
 import (
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func Start() {
-	//define routes
-	http.HandleFunc("/greet", greet)
+func Start(){
+	router := gin.Default()
 
-	//starting server
-	http.ListenAndServe("localhost:8000", nil)
+	//define routes
+	router.GET("/greet", func(c *gin.Context) {
+		c.Header("Content-Type", "application/json")
+		c.JSON(http.StatusOK, gin.H{"Title": "CodeName", "Desc": "Example API Call", "Content": "Hello Jacob and Paul"})
+	})
+	//starting server by default localhost :8080
+	router.Run()
 }
